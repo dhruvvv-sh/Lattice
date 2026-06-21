3c037b6dc8a14999d159f9138cf089ec01e16536
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2026-06-21 21:38:00 +0530

    sample test of recovery.py

---
+++ app/storage/recovery_test.py
@@ -0,0 +1,22 @@
+# app/storage/recovery_test.py
+
+from disk_manager import read_shard
+from erasure import recover_missing_shard
+
+d0 = read_shard("Sample.pdf", 0)
+d1 = read_shard("Sample.pdf", 1)
+d2 = None
+d3 = read_shard("Sample.pdf", 3)
+
+p1 = read_shard("Sample.pdf", 4)
+
+recovered = recover_missing_shard(
+    [d0, d1, d2, d3],
+    p1,
+    missing_index=2
+)
+
+with open("recovered_part2.bin", "wb") as f:
+    f.write(recovered)
+
+print("Recovered shard size:", len(recovered))
