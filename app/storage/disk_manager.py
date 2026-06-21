9427681761103fe77171213738ed7cce22b255f5
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2026-06-21 21:36:35 +0530

    disk manager to write and read the shards and to check if shard exists

---
+++ app/storage/disk_manager.py
@@ -0,0 +1,48 @@
+# app/storage/disk_manager.py
+
+from pathlib import Path
+
+DISKS = [
+    "app/storage/disk1",
+    "app/storage/disk2",
+    "app/storage/disk3",
+    "app/storage/disk4",
+    "app/storage/disk5",
+    "app/storage/disk6",
+]
+
+
+def write_shard(filename, shard_index, data):
+    disk = DISKS[shard_index]
+
+    shard_path = Path(disk) / f"{filename}.part{shard_index}"
+
+    print("Writing to:", shard_path)
+
+    shard_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(shard_path, "wb") as f:
+        f.write(data)
+
+    return str(shard_path)
+
+def read_shard(filename, shard_index):
+    disk = DISKS[shard_index]
+
+    shard_path = Path(disk) / f"{filename}.part{shard_index}"
+
+    print("Trying to read:", shard_path.resolve())
+
+    with open(shard_path, "rb") as f:
+        return f.read()
+
+def shard_exists(filename: str, shard_index: int):
+    """
+    Check if a shard exists.
+    """
+
+    disk = DISKS[shard_index]
+
+    shard_path = Path(disk) / f"{filename}.part{shard_index}"
+
+    return shard_path.exists()
