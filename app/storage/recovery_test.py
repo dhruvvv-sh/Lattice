8e96465f7db4fb7764ee3ee4a188e4e89c973ffb
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2026-06-27 21:18:39 +0530

    update recovery_test.py

--- app/storage/recovery_test.py
+++ app/storage/recovery_test.py
@@ -1,23 +1,29 @@
 # app/storage/recovery_test.py
 
-from disk_manager import read_shard
-from erasure import recover_missing_shard
+from app.storage.disk_manager import read_shard
+from app.storage.erasure import recover_missing_shard
 
-d0 = read_shard("Sample.pdf", 0)
-d1 = read_shard("Sample.pdf", 1)
-d2 = None
-d3 = read_shard("Sample.pdf", 3)
 
-p1 = read_shard("Sample.pdf", 4)
-p2 = read_shard("Sample.pdf", 5)
+def main():
+    d0 = read_shard("Sample.pdf", 0)
+    d1 = read_shard("Sample.pdf", 1)
+    d2 = None
+    d3 = read_shard("Sample.pdf", 3)
 
-recovered = recover_missing_shard(
-    [d0, d1, d2, d3],
-    [p1, p2],
-    missing_index=2
-)
+    p1 = read_shard("Sample.pdf", 4)
+    p2 = read_shard("Sample.pdf", 5)
 
-with open("recovered_part2.bin", "wb") as f:
-    f.write(recovered)
+    recovered = recover_missing_shard(
+        [d0, d1, d2, d3],
+        [p1, p2],
+        missing_index=2
+    )
 
-print("Recovered shard size:", len(recovered))
+    with open("recovered_part2.bin", "wb") as f:
+        f.write(recovered)
+
+    print("Recovered shard size:", len(recovered))
+
+
+if __name__ == "__main__":
+    main()
