551173624ccec593056da7d11a47682b0351a0ee
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2026-06-27 22:31:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.api import buckets,objects
+
+from app.api import buckets, cluster, objects
 from app.database import Base, engine
-from app.api import buckets
 
 app = FastAPI(title="Lattice", version="0.1.0")
 
@@ -10,6 +10,7 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(buckets.router)
 app.include_router(objects.router)
+app.include_router(cluster.router)
 
 @app.get("/health")
 def health():
