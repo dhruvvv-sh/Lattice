c5c1c894c05eae2414f74c191363720a5bb68760
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2026-06-27 22:00:37 +0530

    update heartbeat.py
    updates the disk health status : healthy or not

--- app/storage/heartbeat.py
+++ app/storage/heartbeat.py
@@ -18,8 +18,8 @@ from app.storage.cluster_state import disk_status
 def scan_disks():
     for disk in DISKS:
         healthy = check_disk_health(disk)
-        disk_status[disk] ={
-            "status":"healthy" if healthy else "dead",
-            "last_seen" : datetime.utcnow().isoformat()
+        disk_status[str(disk)] = {
+            "status": "healthy" if healthy else "dead",
+            "last_seen": datetime.utcnow().isoformat()
         }
     return disk_status
