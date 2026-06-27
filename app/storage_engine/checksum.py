284414193a093cf465680136f31ca644b4828f97
dhruvvv-sh <5416899.dhruvsharma@gmail.com>
2026-06-27 21:20:59 +0530

    checksum updated

--- app/storage_engine/checksum.py
+++ app/storage_engine/checksum.py
@@ -1,5 +1,10 @@
 import hashlib
 
+
+def calculate_checksum_bytes(data: bytes):
+    return hashlib.sha256(data).hexdigest()
+
+
 def calculate_checksum(filepath: str):
     sha256 = hashlib.sha256()
     with open(filepath, "rb") as f:
